062bd68d1f760ca767029c99d1dcbe552d14bc78
Theophilus Okwugwuni <theo@Theophiluss-MacBook-Pro.local>
2021-08-19 02:58:12 +0100

    init

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const GOOGLE_API_KEY = "AIzaSyBSHDa4MblU816e2FCtnip3VSP32tYVPP8";
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {required double latitude, required double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY');
+
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
+  }
+}
